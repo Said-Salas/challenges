c201003c6ef7c88a28b56e31972670a2efeae7dc
Said Salas <saidsalastech@gmail.com>
2025-12-26 07:53:59 -0600

    hamming problem finished

--- C/hamming.c
+++ C/hamming.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 #include "utils.h"
 #define MAX_STRAND_LEN 40
 
 int hammingDistance(const char *strandOne, const char *strandTwo) {
     int diff = 0;
-    for (int i = 0; i < strlen(strandOne); i++) {
+    int len = strlen(strandOne);
+    for (int i = 0; i < len; i++) {
         if (!(strandOne[i] == strandTwo[i])) {
             diff++;
         }
@@ -14,16 +16,25 @@ int hammingDistance(const char *strandOne, const char *strandTwo) {
     return diff;
 }
 
-bool onlyCAGT(const char *strand) {
-    for (int i = 0; i < strlen(strand); i++) {
-        if (strand[i] == 'C' || strand[i] == 'A' || strand[i] == 'G' || strand[i] == 'T') {
-            return true;
-        }
-    } 
-    return false;
+bool onlyCAGT(char *strand) {
+    if (isAlphaString(strand)) {
+        int len = strlen(strand);
+        for (int i = 0; i < len; i++) {
+            char c = toupper((unsigned char)strand[i]);
+            if (c != 'C' && c != 'A' && c != 'G' && c != 'T') {
+                fprintf(stderr, "Please enter only bases C, A, G and T.\n");
+                return false;
+                }
+        } 
+        return true;
+    } else {
+        fprintf(stderr, "Please enter only alphabetic values.\n");
+        return false;
+    }
 }
 
 int main(void) {
+    printf("Press CTRL + C to exit program\n");
     printf("Hamming Distance Calculator.\n");
     char strandOne[MAX_STRAND_LEN];
     char strandTwo[MAX_STRAND_LEN];
@@ -32,18 +43,14 @@ int main(void) {
         scanf("%39s", strandOne);
         printf("\nEnter second strand of DNA: ");
         scanf("%39s", strandTwo);
-        if ((strlen(strandOne) == strlen(strandTwo)) && (isAlphaString(strandOne) && isAlphaString(strandTwo))) { 
-            strToUpper(strandOne); strToUpper(strandTwo);
+        if (strlen(strandOne) == strlen(strandTwo)) { 
             if (onlyCAGT(strandOne) && onlyCAGT(strandTwo)) {
+                strToUpper(strandOne), strToUpper(strandTwo);
                 int result = hammingDistance(strandOne, strandTwo);
                 printf("The Hamming Distance between these strands is: %d\n", result);
-                continue;
-            } else {
-                fprintf(stderr, "Please enter only bases C, A, G and T.\n");
-            }
+            } 
         } else {
             fprintf(stderr, "Please enter two equal length strands of DNA.\n");
-            continue;
         }
     }
     return 0;
