8d38338795172603f0d5ffb116e98763f688a9f9
Said Salas <saidsalastech@gmail.com>
2026-01-12 17:37:26 -0600

    binary 1.1

--- C/binary.c
+++ C/binary.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-#include <strlen.h>
-#include <utils.h>
+#include <stdlib.h>
+#include "utils.h"
+#include <string.h>
+#include <math.h>
 #define MAX_LEN 41
 
-int binToDec(const char *str, int len) {
+void binToDec(int *digit, const char *str, int len) {
     for (int i = 0; i < len; i++) {
-        
+        int bin = str[i] - '0';
+        digit += bin * (int)pow(2, len - (i + 1));
     }
 }
 
 bool onlyZeroOne(const char *str, int len) {
     for (int i = 0; i < len; i++) {
-        if (str[i] != '0' || str[i] != '1') {
+        if (str[i] != '0' && str[i] != '1') {
             return false;
         }
     }
@@ -21,19 +24,21 @@ bool onlyZeroOne(const char *str, int len) {
 int main(void) {
     printf("Press Ctrl + C to exit program.\n");
     while (1) {
+        int digit = 0;
         printf("Enter binary number: ");
         char *binary = readInput();
         if (!binary) {
             fprintf(stderr, "Memory error.\n");
             break;
         }
-        int len = strlen(str);
+        int len = strlen(binary);
         if (!isNumeric(binary) || !onlyZeroOne(binary, len)) {
             fprintf(stderr, "Please enter only 0s and 1s.\n");
             free(binary);
             continue;
         }
-        
+        binToDec(&digit, binary, len);
+        printf("Binary %s is number %d\n", binary, digit);
     }
     return 0;
 }
